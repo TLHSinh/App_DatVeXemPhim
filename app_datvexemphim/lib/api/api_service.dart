df3dd2d492cc442f792ee87ca77d4672adad701e
TLHSinh <trinhlehongsinh04@gmail.com>
2025-03-20 05:12:59 +0700

    upgrade giao diện & api đánh giá

--- app_datvexemphim/lib/api/api_service.dart
+++ app_datvexemphim/lib/api/api_service.dart
@@ -5,7 +5,7 @@ class ApiService {
   static final Dio _dio = Dio(
     BaseOptions(
       baseUrl: //"http://localhost:5000/api/v1",
-          "http://10.21.8.141:5000/api/v1",
+          "http://192.168.12.110:5000/api/v1",
       // "http://192.168.1.11:5000/api/v1",
 
       connectTimeout: const Duration(seconds: 10),
