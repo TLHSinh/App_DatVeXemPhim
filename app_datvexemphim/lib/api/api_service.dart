412844c027d9ad0e17cf8c96be3054daa582f4e2
KimAnh2124 <kimanh0165359@gmail.com>
2025-03-20 16:51:00 +0700

    fix

--- app_datvexemphim/lib/api/api_service.dart
+++ app_datvexemphim/lib/api/api_service.dart
@@ -4,8 +4,8 @@ class ApiService {
   static final Dio _dio = Dio(
     BaseOptions(
       baseUrl: 
-      // "http://localhost:5000/api/v1",
-          "http://10.21.10.156:5000/api/v1",
+      "http://localhost:5000/api/v1",
+          // "http://10.21.10.156:5000/api/v1",
       // "http://192.168.1.11:5000/api/v1",
 
       connectTimeout: const Duration(seconds: 10),
