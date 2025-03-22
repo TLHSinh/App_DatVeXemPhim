046097bb9eff566a4786c790c9bf12466f0402fe
melody <hoangdoan103@gmail.com>
2025-03-22 10:14:21 +0700

    fetch [geo_lat,  geo_long]

--- backend/controllers/cinemaController.js
+++ backend/controllers/cinemaController.js
@@ -4,74 +4,76 @@ import RapPhim from "../models/RapPhimSchema.js";
 
 //Hàm fetch và lưu rạp phim vào MongoDB
 export const fetchAndSaveCinemas = async (req, res) => {
-    try {
-        const response = await axios.get(process.env.API_CINEMA);
-        let rapchieuphims = response.data;
+  try {
+    const response = await axios.get(process.env.API_CINEMA);
+    let rapchieuphims = response.data;
 
-        const filteredCinemas = rapchieuphims.filter(cinema =>
-            cinema.name && cinema.name.toUpperCase().includes("CGV")
-        );
+    const filteredCinemas = rapchieuphims.filter(cinema =>
+      cinema.name && cinema.name.toUpperCase().includes("CGV")
+    );
 
-        for (const rapchieuphim of filteredCinemas) {
-            await RapPhim.updateOne(
-                { ma_rap: rapchieuphim.id },
-                {
-                    $set: {
-                        ten_rap: rapchieuphim.name,
-                        dia_chi: rapchieuphim.address,
-                        so_dien_thoai: rapchieuphim.phone,
-                        anh: rapchieuphim.image
-                    }
-                },
-                { upsert: true }
-            );
-        }
-
-        res.json({ message: "Cinemas successfully saved to MongoDB!" });
-    } catch (error) {
-        console.error('Error fetching or saving cinemas:', error);
-        res.status(500).json({ message: 'Error fetching or saving cinemas' });
+    for (const rapchieuphim of filteredCinemas) {
+      await RapPhim.updateOne(
+        { ma_rap: rapchieuphim.id },
+        {
+          $set: {
+            ten_rap: rapchieuphim.name,
+            dia_chi: rapchieuphim.address,
+            so_dien_thoai: rapchieuphim.phone,
+            anh: rapchieuphim.image,
+            geo_lat: rapchieuphim.geo_lat,
+            geo_long: rapchieuphim.geo_long
+          }
+        },
+        { upsert: true }
+      );
     }
+
+    res.json({ message: "Cinemas successfully saved to MongoDB!" });
+  } catch (error) {
+    console.error('Error fetching or saving cinemas:', error);
+    res.status(500).json({ message: 'Error fetching or saving cinemas' });
+  }
 }
 
 //Hàm lấy danh sách rạp phim từ MongoDB
 export const getAllCinemas = async (req, res) => {
-    try {
-        const rapphims = await RapPhim.find();
-        res.json(rapphims);
-        // const movies = await MovieModel.find();
-        // res.json(movies);
-    } catch (error) {
-        res.status(500).json({ message: 'Error retrieving movies' });
-    }
+  try {
+    const rapphims = await RapPhim.find();
+    res.json(rapphims);
+    // const movies = await MovieModel.find();
+    // res.json(movies);
+  } catch (error) {
+    res.status(500).json({ message: 'Error retrieving movies' });
+  }
 };
 
 export const getAllSeatCinemas = async (req, res) => {
-    try {
-        const { id_rap } = req.params;
-        
-        // Kiểm tra xem id_rap có hợp lệ không
-        if (!mongoose.Types.ObjectId.isValid(id_rap)) {
-          return res.status(400).json({ message: "ID rạp không hợp lệ" });
-        }
-    
-        // Lấy danh sách phòng chiếu của rạp
-        const phongChieus = await PhongChieu.find({ id_rap });
-        if (!phongChieus.length) {
-          return res.status(404).json({ message: "Không tìm thấy phòng chiếu trong rạp này" });
-        }
-    
-        // Lấy danh sách ghế từ các phòng chiếu
-        const danhSachGhe = await Ghe.find({ 
-          id_phong: { $in: phongChieus.map(pc => pc._id) }
-        });
-    
-        res.status(200).json({ danhSachGhe });
-      } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: "Lỗi server" });
-      }
+  try {
+    const { id_rap } = req.params;
+
+    // Kiểm tra xem id_rap có hợp lệ không
+    if (!mongoose.Types.ObjectId.isValid(id_rap)) {
+      return res.status(400).json({ message: "ID rạp không hợp lệ" });
     }
+
+    // Lấy danh sách phòng chiếu của rạp
+    const phongChieus = await PhongChieu.find({ id_rap });
+    if (!phongChieus.length) {
+      return res.status(404).json({ message: "Không tìm thấy phòng chiếu trong rạp này" });
+    }
+
+    // Lấy danh sách ghế từ các phòng chiếu
+    const danhSachGhe = await Ghe.find({
+      id_phong: { $in: phongChieus.map(pc => pc._id) }
+    });
+
+    res.status(200).json({ danhSachGhe });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Lỗi server" });
+  }
+}
 
 
 
